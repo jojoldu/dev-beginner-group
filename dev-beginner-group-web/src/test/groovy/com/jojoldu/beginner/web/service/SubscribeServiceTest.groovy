f3823c5f2e80c2dcd48a9424a49d859956dcea6f
jojoldu <jojoldu@gmail.com>
2017-11-19 12:23:37 +0900

    이메일 인증 받기 기능 성공/실패 테스트 코드 완성

--- dev-beginner-group-web/src/test/groovy/com/jojoldu/beginner/web/service/SubscribeServiceTest.groovy
+++ dev-beginner-group-web/src/test/groovy/com/jojoldu/beginner/web/service/SubscribeServiceTest.groovy
@@ -1,8 +1,16 @@
 package com.jojoldu.beginner.web.service
 
+import com.jojoldu.beginner.domain.subscriber.Subscriber
+import com.jojoldu.beginner.domain.subscriber.SubscriberRepository
+import com.jojoldu.beginner.web.exception.InvalidParameterException
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import spock.lang.Specification
+
+import java.time.LocalDate
+
+import static org.mockito.BDDMockito.given
 
 /**
  * Created by jojoldu@gmail.com on 2017. 11. 16.
@@ -16,6 +24,13 @@ class SubscribeServiceTest extends Specification {
     @Autowired
     SubscribeService subscribeService
 
+    @Autowired
+    SubscriberRepository subscriberRepository
+
+    def cleanup() {
+       subscriberRepository.deleteAll()
+    }
+
     def "구독하면 해당 이메일로 인증 메일이 도착함" () {
         given:
         String email = "admin@devbeginner.com"
@@ -25,5 +40,32 @@ class SubscribeServiceTest extends Specification {
 
         then:
         result == true
+    }
+
+    def "인증메일의 링크를 클릭하면 구독 완료됨" () {
+        given:
+        String email = "admin@devbeginner.com"
+        String message = "devbeginner"
+        subscriberRepository.save(new Subscriber(email, LocalDate.now(), message))
+
+        when:
+        subscribeService.certifyComplete(email, message)
+
+        then:
+        Subscriber subscriber = subscriberRepository.findOne(1L)
+        subscriber.isCertified() == true
+    }
+
+    def "인증메일의 링크가 잘못되면 오류 발생" () {
+        given:
+        String email = "admin@devbeginner.com"
+        String message = "devbeginner"
+        subscriberRepository.save(new Subscriber(email, LocalDate.now(), message))
+
+        when:
+        subscribeService.certifyComplete(email, "aa")
+
+        then:
+        def e = thrown(InvalidParameterException.class)
     }
 }
