bb89c02da600b4da174977d6f946e5089f80f0ac
jojoldu <jojoldu@gmail.com>
2017-11-21 15:27:20 +0900

    파일 업로드 테스트 코드 완성

---
+++ dev-beginner-group-static-uploader/src/test/groovy/com/jojoldu/staticuploader/aws/StaticUploaderTest.groovy
@@ -0,0 +1,44 @@
+package com.jojoldu.staticuploader.aws
+
+import spock.lang.Specification
+
+import java.time.LocalDate
+import java.util.stream.Collectors
+import java.util.stream.Stream
+
+/**
+ * Created by jojoldu@gmail.com on 2017. 11. 21.
+ * Blog : http://jojoldu.tistory.com
+ * Github : https://github.com/jojoldu
+ */
+
+class StaticUploaderTest extends Specification {
+
+    def "파일을 읽어 S3에 전송한다" () {
+        given:
+        File file = new File(getTestImagePath())
+        StaticUploader staticUploader = new StaticUploader()
+
+        when:
+        staticUploader.upload(file, "테스트파일"+LocalDate.now().toString()+".jpg")
+
+        then:
+        true
+    }
+
+    def "현재위치 출력"() {
+        expect:
+        println System.getProperty("user.dir")
+        String filePath = getTestImagePath()
+        println filePath
+
+    }
+
+    private String getTestImagePath() {
+        String dir = System.getProperty("user.dir")
+        String filePath = Stream.of(dir, "src", "test", "resources", "static", "image", "test.jpg")
+                .collect(Collectors.joining(File.separator))
+        filePath
+    }
+
+}
---
+++ dev-beginner-group-static-uploader/src/test/java/com/jojoldu/StaticUploaderApplicationTests.java
@@ -0,0 +1,6 @@
+package com.jojoldu;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaticUploaderApplicationTests { }
