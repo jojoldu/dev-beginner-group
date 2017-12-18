2c42137d750957c91c8de779267b664c42814fbb
jojoldu <jojoldu@gmail.com>
2017-12-18 21:04:52 +0900

    통계관련 엔티티 추가

---
+++ dev-beginner-group-core/src/main/java/com/jojoldu/beginner/domain/statistics/mail/MailLinkClick.java
@@ -0,0 +1,39 @@
+package com.jojoldu.beginner.domain.statistics.mail;
+
+import com.jojoldu.beginner.domain.BaseTimeEntity;
+import com.jojoldu.beginner.domain.letter.Letter;
+import com.jojoldu.beginner.domain.letter.LetterContent;
+import com.jojoldu.beginner.domain.subscriber.Subscriber;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+/**
+ * Created by jojoldu@gmail.com on 2017. 12. 17.
+ * Blog : http://jojoldu.tistory.com
+ * Github : https://github.com/jojoldu
+ */
+
+@NoArgsConstructor
+@Entity
+public class MailLinkClick extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "subscriber_id", foreignKey = @ForeignKey(name = "FK_MAIL_OPEN_SUBSCRIBER"))
+    private Subscriber subscriber;
+
+    @ManyToOne
+    @JoinColumn(name = "letter_content_id", foreignKey = @ForeignKey(name = "FK_MAIL_OPEN_LETTER_CONTENT"))
+    private LetterContent letterContent;
+
+    @Builder
+    public MailLinkClick(Subscriber subscriber, LetterContent letterContent) {
+        this.subscriber = subscriber;
+        this.letterContent = letterContent;
+    }
+}
---
+++ dev-beginner-group-core/src/main/java/com/jojoldu/beginner/domain/statistics/mail/MailLinkClickRepository.java
@@ -0,0 +1,12 @@
+package com.jojoldu.beginner.domain.statistics.mail;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by jojoldu@gmail.com on 2017. 12. 17.
+ * Blog : http://jojoldu.tistory.com
+ * Github : https://github.com/jojoldu
+ */
+
+public interface MailLinkClickRepository extends JpaRepository<MailLinkClick, Long>{
+}
