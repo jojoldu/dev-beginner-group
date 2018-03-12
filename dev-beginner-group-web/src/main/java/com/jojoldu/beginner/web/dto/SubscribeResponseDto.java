8aa9cd8743f2a5f6a946e2fafe26eff5d4ca190f
jojoldu <jojoldu@gmail.com>
2018-03-12 11:09:46 +0900

    깨진 테스트 수정

--- dev-beginner-group-web/src/main/java/com/jojoldu/beginner/web/dto/SubscribeResponseDto.java
+++ dev-beginner-group-web/src/main/java/com/jojoldu/beginner/web/dto/SubscribeResponseDto.java
@@ -1,6 +1,8 @@
 package com.jojoldu.beginner.web.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
  * Created by jojoldu@gmail.com on 2017. 11. 17.
@@ -8,6 +10,8 @@ import lombok.AllArgsConstructor;
  * Github : https://github.com/jojoldu
  */
 
+@Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class SubscribeResponseDto {
 
