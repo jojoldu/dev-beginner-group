a776eb9a775c2315c0ef2d035e2327a419e2a0b3
jojoldu <jojoldu@gmail.com>
2018-05-05 11:03:23 +0900

    travis ci 재연동 - 간단 콘솔 출력 먼저

---
+++ scripts/before/code-deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /home/ec2-user/app/script
+deploy.sh > /dev/null 2> /dev/null < /dev/null &
--- scripts/code-deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /home/ec2-user/app/script
-./deploy.sh > /dev/null 2> /dev/null < /dev/null &
