e796a68ee1c698d72578b659acad92fa769d8df8
jojoldu <jojoldu@gmail.com>
2018-05-14 13:09:31 +0900

    오류 수정

--- dev-beginner-group-web/src/main/java/com/jojoldu/beginner/web/config/WebProperties.java
+++ dev-beginner-group-web/src/main/java/com/jojoldu/beginner/web/config/WebProperties.java
@@ -1,6 +1,7 @@
 package com.jojoldu.beginner.web.config;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
