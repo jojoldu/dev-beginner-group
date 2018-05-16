c42d495a55b816f82ed07889015fa92573122d7e
jojoldu <jojoldu@gmail.com>
2018-05-16 22:17:21 +0900

    switch에서도 헬스체크 추가

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -17,4 +17,17 @@ function switch()
 
     echo "> Nginx Reload"
     sudo service nginx reload
+
+    checkSwitch
+}
+
+function checkSwitch
+{
+    response=$(curl -s http://localhost/actuator/health)
+    up_count=$(echo ${response} | grep 'UP' | wc -l)
+    if [ ${up_count} -eq 0 ]
+    then # $up_count == 0 ("UP" 문자열이 없으면)
+        echo "> Switch 실패"
+        exit 1
+    fi
 }
