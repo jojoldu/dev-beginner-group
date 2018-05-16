a3a5c9c867c3e6414d291a6a4a55ebbecf4f9df8
jojoldu <jojoldu@gmail.com>
2018-05-16 21:00:31 +0900

    admin 깨진 테스트 수정

--- dev-beginner-group-admin/src/main/java/com/jojoldu/beginner/AdminApplication.java
+++ dev-beginner-group-admin/src/main/java/com/jojoldu/beginner/AdminApplication.java
@@ -1,16 +1,8 @@
 package com.jojoldu.beginner;
 
-import com.zaxxer.hikari.HikariDataSource;
-import org.h2.tools.Server;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-import javax.sql.DataSource;
-import java.sql.SQLException;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -24,13 +16,5 @@ public class AdminApplication {
         new SpringApplicationBuilder(AdminApplication.class)
                 .properties(APPLICATION_LOCATIONS)
                 .run(args);
-    }
-
-    @Bean
-    @Profile("local")
-    @ConfigurationProperties("spring.datasource") // yml의 설정값을 Set한다.
-    public DataSource dataSource() throws SQLException {
-        Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092").start();
-        return new HikariDataSource();
     }
 }
