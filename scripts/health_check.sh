487336c89656fd47e4c57b7ced21f35e00881eed
jojoldu <jojoldu@gmail.com>
2018-05-16 21:27:37 +0900

    BeforeAllowTraffic 생명주기 안되서 제외

--- scripts/health_check.sh
+++ scripts/health_check.sh
@@ -3,6 +3,7 @@
 ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
+source ${ABSDIR}/switch.sh
 
 idle_port=$(find_idle_port)
 
@@ -35,3 +36,5 @@ do
   echo "> Health check 연결 실패. 재시도..."
   sleep 10
 done
+
+switch
--- scripts/switch.sh
+++ scripts/switch.sh
@@ -4,14 +4,17 @@ ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
-idle_port=$(find_idle_port)
+function switch()
+{
+    idle_port=$(find_idle_port)
 
-echo "> 전환할 Port: $idle_port"
-echo "> Port 전환"
-echo "set \$service_url http://127.0.0.1:${idle_port};" |sudo tee /etc/nginx/conf.d/service-url.inc
+    echo "> 전환할 Port: $idle_port"
+    echo "> Port 전환"
+    echo "set \$service_url http://127.0.0.1:${idle_port};" |sudo tee /etc/nginx/conf.d/service-url.inc
 
-current_profile=$(curl -s http://localhost/profile)
-echo "> Nginx Current Profile : $current_profile"
+    current_profile=$(curl -s http://localhost/profile)
+    echo "> Nginx Current Profile : $current_profile"
 
-echo "> Nginx Reload"
-sudo service nginx reload
+    echo "> Nginx Reload"
+    sudo service nginx reload
+}
